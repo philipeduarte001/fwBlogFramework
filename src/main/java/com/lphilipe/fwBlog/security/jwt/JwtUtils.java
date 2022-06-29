02ffc0a727521b85ea8af7af18e421803c9bc22b
Luiz Philipe <philipeduarte001@gmail.com>
2022-06-28 22:22:32 -0300

    atualização do jsonwebtoken

--- src/main/java/com/lphilipe/fwBlog/security/jwt/JwtUtils.java
+++ src/main/java/com/lphilipe/fwBlog/security/jwt/JwtUtils.java
@@ -1,11 +1,12 @@
 package com.lphilipe.fwBlog.security.jwt;
 import com.lphilipe.fwBlog.security.services.UserDetailsImpl;
-import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.*;
 
 import java.util.Date;
 
